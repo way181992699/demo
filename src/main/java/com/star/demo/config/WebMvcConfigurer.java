36e7d455511bf3ee0912ca2ffd539b59e0da5179
Administrator <1273870341@163.com>
2018-06-29 12:08:08 +0800

    添加interceptor拦截器

---
+++ src/main/java/com/star/demo/config/WebMvcConfigurer.java
@@ -0,0 +1,30 @@
+package com.star.demo.config;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.star.demo.controller.interceptor.FristInterceptor;
+import com.star.demo.controller.interceptor.SecondInterceptor;
+/***
+ * 在SpringBoot2.0及Spring 5.0 WebMvcConfigurerAdapter已被废弃，目前找到解决方案就有两种:
+ * 1 直接实现WebMvcConfigurer接口
+ * 2 直接继承WebMvcConfigurationSupport
+ * @author Administrator
+ *
+ */
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurationSupport {
+
+	
+	@Override
+	protected void addInterceptors(InterceptorRegistry registry) {
+		// 拦截器会按照注册的顺序执行
+		registry.addInterceptor(new FristInterceptor()).addPathPatterns("/user/**");
+		registry.addInterceptor(new SecondInterceptor()).addPathPatterns("/MyWife/**");
+		super.addInterceptors(registry);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/star/demo/config/WebMvcConfigurer2.java
@@ -0,0 +1,26 @@
+package com.star.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.star.demo.controller.interceptor.FristInterceptor;
+
+/***
+ * 在SpringBoot2.0及Spring 5.0 WebMvcConfigurerAdapter已被废弃，目前找到解决方案就有两种:
+ * 1 直接实现WebMvcConfigurer接口
+ * 2 直接继承WebMvcConfigurationSupport
+ * @author Administrator
+ *
+ */
+//@Configuration
+public class WebMvcConfigurer2 implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new FristInterceptor()).addPathPatterns("/*/**");
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+
+
+
+}
